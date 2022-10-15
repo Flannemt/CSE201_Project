16945fcf1b4059397a0709cdbe1d4537892f716d
Edmier <94007463+Edmier@users.noreply.github.com>
2022-10-14 21:19:30 -0400

    Basic header and footer

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { navigating } from '$app/stores';
+
+	const currentlyNavigating = $navigating !== null ? true : false;
+</script>
+
+<nav>
+	<a href="/" class="logo">SvelteKit</a>
+	<ul>
+		<li><a href="/" disabled={currentlyNavigating}>Home</a></li>
+		<li><a href="/about" disabled={currentlyNavigating}>About</a></li>
+	</ul>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 0 1em;
+		height: 4em;
+		background: #ff3e00;
+		color: #fff;
+	}
+
+	.logo {
+		font-weight: 600;
+	}
+
+	ul {
+		display: flex;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		margin: 0 0.5em;
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,17 @@
 <script>
-	import Header from './Header.svelte';
+	import Header from '$comp/header.svelte';
+	import Footer from '$comp/footer.svelte';
+
 	import './styles.css';
 </script>
 
 <div class="app">
+	<!-- Insert our header -->
 	<Header />
 
-	<main>
-		<slot />
-	</main>
+	<!-- Page data gets inserted here -->
+	<slot />
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<!-- Insert our footer -->
+	<Footer />
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
