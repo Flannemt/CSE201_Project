a3ca94e80f34e7e39d404cae2871238852973ed9
Edmier <94007463+Edmier@users.noreply.github.com>
2022-10-30 23:44:10 -0400

    Login logic

---
+++ src/routes/login/+page.ts
@@ -0,0 +1,23 @@
+import { PUBLIC_DISCORD_CLIENT_ID as CLIENT_ID } from '$env/static/public';
+import { browser } from '$app/environment';
+import { redirect } from '@sveltejs/kit';
+
+import type { PageLoad } from './$types';
+export const load: PageLoad = ({ url }) => {
+	const success = url.searchParams.get('success');
+
+	if (browser && !success) {
+		const endpoint =
+			'https://discord.com/api/oauth2/authorize' +
+			`?client_id=${CLIENT_ID}` +
+			'&redirect_uri=' +
+			encodeURIComponent(url.origin + '/login/callback') +
+			'&response_type=code&scope=identify';
+
+		throw redirect(303, endpoint);
+	}
+
+	return {
+		success: success === 'true'
+	};
+};
