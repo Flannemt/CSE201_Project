41ff28a48bac7c742cc9eaddd24ff2c9236ea18f
Edmier <94007463+Edmier@users.noreply.github.com>
2022-10-30 23:46:26 -0400

    Show list of threads/create new

---
+++ src/routes/threads/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { ActionData, PageData } from './$types';
+
+	export let data: PageData;
+	export let action: ActionData;
+
+	const { threads } = data;
+</script>
+
+<section>
+	{#each threads as thread}
+		<div class="thread">
+			<a href="/threads/{thread?.id}">{thread?.id}</a>
+		</div>
+	{/each}
+</section>
+
+<section>
+	<form method="POST" action="?/create">
+		<!-- <input name="name" type="text"> -->
+		<button>Create Thread</button>
+	</form>
+	{#if action?.success}
+		<p>Success! Created new thread here</p>
+		<a href="/threads/{action.thread.id}">Link to new thread</a>
+	{/if}
+</section>
