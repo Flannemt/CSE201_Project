21eb148adc6ef4507bc7c16c185917b299eadb93
Edmier <94007463+Edmier@users.noreply.github.com>
2022-11-09 20:14:29 -0500

    Message Model

--- src/database/models/message.ts
+++ src/database/models/message.ts
@@ -1,9 +1,49 @@
+import { Model, type InferAttributes, type InferCreationAttributes, type CreationOptional, Sequelize, DataTypes, type Optional, type WhereAttributeHash } from 'sequelize';
 import type { Snowflake } from './user';
 
-export interface Message {
-	id: Snowflake;
-	author: Snowflake;
-	thread: Snowflake;
-	content: string;
-	timestamp: number;
+export class Message extends Model<InferAttributes<Message>, InferCreationAttributes<Message>> {
+	// Default
+	declare createdAt: CreationOptional<Date>;
+	declare updatedAt: CreationOptional<Date>;
+
+	// Added
+	declare uuid: Snowflake;
+	declare author: Snowflake;
+	declare thread: Snowflake;
+	declare content: string;
 }
+
+export function MessagesInit(sequelize: Sequelize) {
+	Message.init(
+		{
+			uuid: {
+				type: DataTypes.STRING,
+				unique: true,
+				primaryKey: true,
+				allowNull: false,
+			},
+
+			author: DataTypes.STRING,
+			thread: DataTypes.STRING,
+			content: DataTypes.STRING,
+
+			createdAt: DataTypes.DATE,
+			updatedAt: DataTypes.DATE,
+		},
+		{
+			sequelize: sequelize,
+			tableName: 'messages',
+			freezeTableName: true,
+		}
+	);
+
+	return Message;
+}
+
+// No idea if this is how you're supposed to do it, but it works?
+export type MessageWhereOptions = WhereAttributeHash<Message>;
+
+type MessageCreationOptions = InferCreationAttributes<Message>;
+export type MessageUpdateOptions = Optional<MessageCreationOptions, keyof MessageCreationOptions>;
+
+export type MessageData = Message;
