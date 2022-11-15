04270ce3d579ee423aec6687b46c2a66aecbc984
Flannemt <89662193+Flannemt@users.noreply.github.com>
2022-11-14 21:17:08 -0500

    Add files via upload

--- src/routes/threads/[threadId]/+page.svelte
+++ src/routes/threads/[threadId]/+page.svelte
@@ -1,21 +1,78 @@
 <script lang="ts">
 	import type { DiscordUser } from '$db/models/user';
 	import type { ActionData, PageData } from './$types';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
 
 	const { thread, members } = data;
 	const memberMap = new Map<string, DiscordUser>();
 
+	console.log(data.thread.messages);
+
 	for (const member of members) {
 		memberMap.set(member.uuid, member.user);
 	}
+
+	let url = '';
+
+	//var count = data.thread.messages.length; //counter var
+
+	onMount(() => url = $page.url.pathname);
+
+	onMount(async () => {
+	setInterval(fetchMessages, 1000);
+	});
+
+	$: messages = data.thread.messages;
+
+	async function fetchMessages() {
+		let count = messages.length; //setting again to update every fetch
+		let message = null;
+		const request = await fetch(window.location.origin + url + '/message/?count=' + count);
+		message = await request.json();
+
+		console.log(message.message);
+		console.log(count);
+		
+		if(count != message.count) {
+			//messages = [...messages, ...message.updates];
+			//messages.push(message.updates);
+
+			if(count != message.count && message != undefined) {
+				messages = [...messages, ...(message.updates ?? [])];
+			//messages.push(message.updates);
+			
+				message.message = 'All good'; //resets messages pkt
+				count = message.count;
+			/*
+			let text = document.querySelector("#messageText")
+			if(text.value == '') {
+				let cancel;
+				text.value = localStorage.getItem("val")
+
+
+				text.addEventListener("keyup", event => {
+				if (cancel) clearTimeout(cancel)
+					cancel = setTimeout(() => {
+					localStorage.setItem("val", event.target.value)
+				}, 1000)		
+})
+*/
+	}
+			//location.reload(); 
+			//count = message.count; //sets count to what the server has
+			//message = null; //resets messages pkt
+}
+}
+
 </script>
 
 <main>
 	<section class="messages">
 		<div>
-			{#each data.thread.messages ?? [] as message (message.uuid)}
+			{#each messages ?? [] as message (message.uuid)}
 				<div class="message">
 					<h5>
 						{memberMap.get(message.author)?.username}
@@ -27,7 +84,7 @@
 					</h5>
 					<!-- button for adding friends here -->
 					<form method="POST" action="?/friend">
-						<input name="friendId" type="text" value={message.author} hidden/>
+						<input name="friendId" type="text" value={message.author} hidden />
 						<button>Send</button>
 					</form>
 					<p>{message.content}</p>
@@ -38,7 +95,7 @@
 
 	<section class="inputs">
 		<form method="POST" action="?/message">
-			<input class="chatbox" name="content" type="text" />
+			<input class="chatbox" name="content" type="text" value="" id='messageText'/> 
 			<button>Send</button>
 		</form>
 		<form method="POST" action="?/invite">
